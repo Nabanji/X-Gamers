b62d90eeef211289fc7b6698e53b9f1b100e79b4
Nabanji <pnabanji@gmail.com>
2026-09-10 17:35:36 +0300

    Initial commit

---
+++ client/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-rose-500">
+      <Text className="text-white text-2xl font-bold">Hello, World!</Text>
+    </View>
+  );
+}
